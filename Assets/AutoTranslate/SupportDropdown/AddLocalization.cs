57da51164e45b6dd246fac3d53aba23b0bde2832
Maksym Hernets <gernets.maksim@gmail.com>
2022-10-06 23:16:22 +0300

    Separation adding localization

---
+++ Assets/AutoTranslate/SupportDropdown/AddLocalization.cs
@@ -0,0 +1,47 @@
+using GoodTime.HernetsMaksym.AutoTranslate.Editor;
+using GoodTime.Tools.Helpers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Tables;
+using UnityEngine.UI;
+using static UnityEngine.Localization.Tables.SharedTableData;
+
+namespace GoodTime.HernetsMaksym.AutoTranslate.SupportDropdown
+{
+    public static partial class AddLocalization
+    {
+        public static partial void Execute(AddLocalizationParameters parameters, StatusLocalizationScene statusLocalizationScene);
+
+        public static partial void Execute(AddLocalizationParameters parameters, StatusLocalizationScene statusLocalizationScene)
+        {
+            SharedTableData sharedTable = SharedTableDataExtension.GetOrAdd_SharedTableData(parameters.NameTable);
+            StringTable stringTable = SimpleInterfaceStringTable.GetStringTable(sharedTable, parameters.SourceLocale);
+
+            if (parameters.Lists.ContainsKey("Dropdown") && parameters.Lists["Dropdown"])
+            {
+                //AddLocalization_DropdownLegacy(statusLocalizationScene.TextMeshs, stringTable, sharedTable);
+            }
+        }
+
+        private static void AddLocalization_DropdownLegacy(List<Dropdown> dropdowns, StringTable stringTable, SharedTableData sharedTable)
+        {
+            LocalizeStringEvent localizeStringEvent = default(LocalizeStringEvent);
+            SharedTableEntry sharedTableEntry = default(SharedTableEntry);
+
+            foreach (Dropdown text in dropdowns)
+            {
+                localizeStringEvent = LocalizeStringEventExtension.GetOrAdd_LocalizeStringEventComponent(text.gameObject);
+
+                sharedTableEntry = SharedTableDataExtension.SharedTableData_AddEntry(sharedTable, text.gameObject.name, text.transform.parent?.name, "Dropdown");
+
+                stringTable.AddEntry(sharedTableEntry.Key, text.GetLineOptions());
+
+                localizeStringEvent.Clear_OnUpdateString();
+                localizeStringEvent.Sign_ReferenceTable(sharedTable.TableCollectionName, sharedTableEntry.Key);
+                localizeStringEvent.Sign_OnUpdateString_DropdownLegacy(text);
+            }
+        }
+    }
+}
---
+++ Assets/AutoTranslate/SupportTextMeshPro/AddLocalization.cs
@@ -0,0 +1,45 @@
+using GoodTime.HernetsMaksym.AutoTranslate.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Tables;
+using static UnityEngine.Localization.Tables.SharedTableData;
+
+namespace GoodTime.HernetsMaksym.AutoTranslate.SupportTextMeshPro
+{
+    public static partial class AddLocalization
+    {
+        public static partial void Execute(AddLocalizationParameters parameters, StatusLocalizationScene statusLocalizationScene);
+
+        public static partial void Execute(AddLocalizationParameters parameters, StatusLocalizationScene statusLocalizationScene)
+		{
+            SharedTableData sharedTable = SharedTableDataExtension.GetOrAdd_SharedTableData(parameters.NameTable);
+            StringTable stringTable = SimpleInterfaceStringTable.GetStringTable(sharedTable, parameters.SourceLocale);
+
+            if (parameters.Lists.ContainsKey("Text Mesh Pro") && parameters.Lists["Text Mesh Pro"])
+            {
+                AddLocalization_TextMeshPro(statusLocalizationScene.TextMeshs, stringTable, sharedTable);
+            }
+        }
+
+        private static void AddLocalization_TextMeshPro(List<TextMeshProUGUI> texts, StringTable stringTable, SharedTableData sharedTable)
+        {
+            LocalizeStringEvent localizeStringEvent = default(LocalizeStringEvent);
+            SharedTableEntry sharedTableEntry = default(SharedTableEntry);
+            foreach (TextMeshProUGUI text in texts)
+            {
+                localizeStringEvent = LocalizeStringEventExtension.GetOrAdd_LocalizeStringEventComponent(text.gameObject);
+
+                sharedTableEntry = SharedTableDataExtension.SharedTableData_AddEntry(sharedTable, text.gameObject.name, text.transform.parent?.name, "TextMeshPro");
+
+                stringTable.AddEntry(sharedTableEntry.Key, text.text);
+
+                localizeStringEvent.Clear_OnUpdateString();
+                localizeStringEvent.Sign_ReferenceTable(sharedTable.TableCollectionName, sharedTableEntry.Key);
+                localizeStringEvent.Sign_OnUpdateString_TextMeshPro(text);
+            }
+        }
+    }
+}
