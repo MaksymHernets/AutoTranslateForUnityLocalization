3caa6caadac81b8bcddf74eca3840b979a8573b5
Maksym Hernets <gernets.maksim@gmail.com>
2022-10-06 23:15:30 +0300

    Code refactoring

--- Assets/AutoTranslate/Scripts/SimpleLocalization/AddLocalization.cs
+++ Assets/AutoTranslate/Scripts/SimpleLocalization/AddLocalization.cs
@@ -15,9 +15,11 @@ using static UnityEngine.Localization.Tables.SharedTableData;
 
 namespace GoodTime.HernetsMaksym.AutoTranslate
 {
-    public static class AddLocalization
+    public static partial class AddLocalization
     {
-        public static string Execute(AddLocalizationParameters parameters, StatusLocalizationScene statusLocalizationScene)
+        public static partial void Execute(AddLocalizationParameters parameters, StatusLocalizationScene statusLocalizationScene);
+
+        public static partial void Execute(AddLocalizationParameters parameters, StatusLocalizationScene statusLocalizationScene)
         {
             SharedTableData sharedTable = SharedTableDataExtension.GetOrAdd_SharedTableData(parameters.NameTable);
             StringTable stringTable = SimpleInterfaceStringTable.GetStringTable(sharedTable, parameters.SourceLocale);
@@ -26,23 +28,18 @@ namespace GoodTime.HernetsMaksym.AutoTranslate
             {
                 AddLocalization_TextLegacy(statusLocalizationScene.LegacyTexts, stringTable, sharedTable);
             }
-            if (parameters.Lists.ContainsKey("Text Mesh Pro") && parameters.Lists["Text Mesh Pro"])
-            {
-                AddLocalization_TextMeshPro(statusLocalizationScene.TextMeshs, stringTable, sharedTable);
-            }
-
-            return "Completed";
         }
 
         private static void AddLocalization_TextLegacy(List<Text> texts, StringTable stringTable, SharedTableData sharedTable)
         {
             LocalizeStringEvent localizeStringEvent = default(LocalizeStringEvent);
             SharedTableEntry sharedTableEntry = default(SharedTableEntry);
+
             foreach (Text text in texts)
             {
-                localizeStringEvent = GetOrAdd_LocalizeStringEventComponent(text.gameObject);
+                localizeStringEvent = LocalizeStringEventExtension.GetOrAdd_LocalizeStringEventComponent(text.gameObject);
 
-                sharedTableEntry = SharedTableData_AddEntry(sharedTable, text.gameObject.name, text.transform.parent?.name, "TextLegacy");
+                sharedTableEntry = SharedTableDataExtension.SharedTableData_AddEntry(sharedTable, text.gameObject.name, text.transform.parent?.name, "TextLegacy");
 
                 stringTable.AddEntry(sharedTableEntry.Key, text.text);
 
@@ -50,64 +47,6 @@ namespace GoodTime.HernetsMaksym.AutoTranslate
                 localizeStringEvent.Sign_ReferenceTable(sharedTable.TableCollectionName, sharedTableEntry.Key);
                 localizeStringEvent.Sign_OnUpdateString_TextLegacy(text);
             }
-        }
-
-        private static void AddLocalization_TextMeshPro(List<TextMeshProUGUI> texts, StringTable stringTable, SharedTableData sharedTable)
-        {
-            LocalizeStringEvent localizeStringEvent = default(LocalizeStringEvent);
-            SharedTableEntry sharedTableEntry = default(SharedTableEntry);
-            foreach (TextMeshProUGUI text in texts)
-            {
-                localizeStringEvent = GetOrAdd_LocalizeStringEventComponent(text.gameObject);
-
-                sharedTableEntry = SharedTableData_AddEntry(sharedTable, text.gameObject.name, text.transform.parent?.name, "TextMeshPro");
-
-                stringTable.AddEntry(sharedTableEntry.Key, text.text);
-
-                localizeStringEvent.Clear_OnUpdateString();
-                localizeStringEvent.Sign_ReferenceTable(sharedTable.TableCollectionName, sharedTableEntry.Key);
-                localizeStringEvent.Sign_OnUpdateString_TextMeshPro(text);
-            }
-        }
-
-        private static void AddLocalization_DropdownLegacy(List<Dropdown> dropdowns, StringTable stringTable, SharedTableData sharedTable)
-        {
-            LocalizeStringEvent localizeStringEvent = default(LocalizeStringEvent);
-            SharedTableEntry sharedTableEntry = default(SharedTableEntry);
-            foreach (Dropdown text in dropdowns)
-            {
-                localizeStringEvent = GetOrAdd_LocalizeStringEventComponent(text.gameObject);
-
-                sharedTableEntry = SharedTableData_AddEntry(sharedTable, text.gameObject.name, text.transform.parent?.name, "TextMeshPro");
-
-                stringTable.AddEntry( sharedTableEntry.Key, text.GetLineOptions() );
-
-                localizeStringEvent.Clear_OnUpdateString();
-                localizeStringEvent.Sign_ReferenceTable(sharedTable.TableCollectionName, sharedTableEntry.Key);
-                localizeStringEvent.Sign_OnUpdateString_DropdownLegacy(text);
-            }
-        }
-
-        private static LocalizeStringEvent GetOrAdd_LocalizeStringEventComponent(GameObject gameObject)
-        {
-            LocalizeStringEvent localizeStringEvent = default(LocalizeStringEvent);
-            if (gameObject.TryGetComponent<LocalizeStringEvent>(out localizeStringEvent))
-                return gameObject.GetComponent<LocalizeStringEvent>();
-            else
-                return gameObject.AddComponent<LocalizeStringEvent>();
-        }
-
-        private static SharedTableEntry SharedTableData_AddEntry(SharedTableData sharedTable, string nameGameObjet, string parentName, string typeText)
-        {
-            string name = String.Format("[{0}][{1}][{2}]", nameGameObjet, parentName, typeText);
-            int variants = 1;
-            while (sharedTable.Contains(name))
-            {
-                name = String.Format("[{0}][{1}][{2}][{3}]", nameGameObjet, parentName, typeText, variants);
-                ++variants;
-            }
-            SharedTableEntry sharedTableEntry = sharedTable.AddKey(name);
-            return sharedTableEntry;
         }
     }
 }
