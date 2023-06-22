e978fcc9a9f76a3e275f6975754e16ab3193c0d4
Maksym Hernets <gernets.maksim@gmail.com>
2023-06-22 14:37:33 +0300

    Added Search Text in Scenes of Prefabs window

---
+++ Assets/AutoTranslate/Editor/Search/SearchTextInPrefabs_EditorWindow.cs
@@ -0,0 +1,96 @@
+using GoodTime.HernetsMaksym.AutoTranslate.Editor;
+using GoodTime.Tools.GUIPro;
+using GoodTime.Tools.Helpers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GoodTime.HernetsMaksym.AutoTranslate.Windows
+{
+    public class SearchTextInPrefabs_EditorWindow : BaseSearch_EditorWindow
+    {
+        // Window parameters
+        private const string k_WindowTitle = "Search Text in Prefabs";
+
+        private CheckListGUI _checkListScenes;
+
+        [MenuItem("Window/Auto Localization/Search Text in Prefabs", false, MyProjectSettings_AutoTranslate.BaseIndex + 42)]
+        public static void ShowWindow()
+        {
+            Type gameview = typeof(UnityEditor.EditorWindow).Assembly.GetType("UnityEditor.GameView");
+            SearchTextInPrefabs_EditorWindow window = GetWindow<SearchTextInPrefabs_EditorWindow>(k_WindowTitle, true, typeof(SceneView), gameview);
+            window.titleContent = new GUIContent(k_WindowTitle, EditorIcons.SearchText);
+            window.Show();
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            List<GameObject> gameObjects = DatabaseProject.GetPrefabs();
+            _checkListScenes = new CheckListGUI(gameObjects.Select(w => w.name).ToList());
+        }
+
+        protected override void OnFocus()
+        {
+            base.OnFocus();
+            List<GameObject> gameObjects = DatabaseProject.GetPrefabs();
+            _checkListScenes.UpdateCheck(gameObjects.Select(w => w.name).ToList(), false);
+        }
+
+        private void OnGUI()
+        {
+            ShowNameWindow(k_WindowTitle);
+
+            EditorGUILayout.BeginHorizontal(GUILayout.ExpandWidth(true), GUILayout.MinHeight(170)); // Main Begin 
+            EditorGUILayout.BeginFadeGroup(1); // Begin 0
+            _checkListScenes.DrawButtons("Scenes:");
+            EditorGUILayout.EndFadeGroup();
+            EditorGUILayout.BeginFadeGroup(2); // Begin 1
+            _skipPrefab = LinesGUI.DrawLineToggle("Skip prefabs", _skipPrefab);
+            _skipEmptyText = LinesGUI.DrawLineToggle("Skip empty text", _skipEmptyText);
+            _removeMissStringEvents = LinesGUI.DrawLineToggle("Remove miss stringEvents", _removeMissStringEvents);
+            _autoSave = LinesGUI.DrawLineToggle("Auto Save", _autoSave);
+
+            EditorGUILayout.HelpBox("Not working yet", MessageType.Error);
+            GUI.enabled = false;
+            if (GUILayout.Button("Add localization"))
+            {
+                StartAddLocalization();
+            }
+            EditorGUILayout.EndFadeGroup();
+            EditorGUILayout.EndHorizontal();
+        }
+
+        private string StartAddLocalization()
+        {
+            //AddLocalizationParameters parameters = new AddLocalizationParameters();
+
+            //if (_dropdownTables.Selected == KEYWORD_NEWTABLE) parameters.NameTable = _nameTable;
+            //else parameters.NameTable = _dropdownTables.Selected;
+
+            //if (string.IsNullOrEmpty(parameters.NameTable)) return "nameTable is null";
+
+            //parameters.IsSkipPrefab = _skipPrefab;
+            //parameters.IsSkipEmptyText = _skipEmptyText;
+            //parameters.SourceLocale = _selectedLocale;
+            //parameters.Lists = _checkListSearchElements.GetElements();
+
+            //if (_statusLocalizationScene == null) StartSearch();
+            //else GetCheckTable();
+
+            //AddLocalization.Execute(parameters, _statusLocalizationScene);
+            //if (_removeMissStringEvents) AddLocalization.RemoveMiss_LocalizeStringEvent(_statusLocalizationScene.LocalizeStringEvents);
+            //if (_autoSave)
+            //{
+            //    EditorSceneManager.SaveOpenScenes();
+            //    EditorUtility.SetDirty(_prefabStage.prefabContentsRoot);
+            //}
+
+            return "Completed";
+        }
+    }
+}
